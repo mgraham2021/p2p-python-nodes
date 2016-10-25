337a1c764475cbc2dd7c2d5d80621ca2752fff3a
Michael Graham <michaelg@6ft.com>
2016-10-25 05:02:56 -0500

    more modules to include/beginnings of the multiprocess bit

---
+++ node/__main__.py
@@ -0,0 +1,4 @@
+from node import main
+
+main.main()
+
--- node/main.py
+++ node/main.py
@@ -1,6 +1,26 @@
+
+import os
+from optparse import OptionParser
+
 from pyp2p.net import *
 from pyp2p.unl import UNL
 from pyp2p.dht_msg import DHT
 import time
 
 
+
+
+import settings
+
+
+def main(*args, **kwargs):
+    # args setup
+    parser = OptionParser()
+    parser.add_option('-i', '--id', dest='identifier',
+                      help='The identifier for this instance; Used when spawning '
+                           'mulitple workers that can be load balanced.',
+                      type=int,
+                      default=0)
+    (options, parsed_args) = parser.parse_args()
+
+    newpid = os.fork()
