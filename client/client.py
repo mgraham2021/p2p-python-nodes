d29eed61ead5977334038290a5c6a3dc71411fa0
Michael <michaelg@6ft.com>
2016-10-30 21:20:50 -0500

    more better, client idea

--- client/client.py
+++ client/client.py
@@ -10,41 +10,45 @@ from pyp2p.dht_msg import DHT
 import time
 
 
-#Start Alice's direct server.
-alice_dht = DHT()
-alice_direct = Net(passive_bind="192.168.0.45", passive_port=44444, interface="eth0:2", net_type="direct", dht_node=alice_dht, debug=1)
-alice_direct.start()
+# Start existing's server direct connection.
+node_dht = DHT()
+node_direct = Net(passive_bind="192.168.0.45", passive_port=44444, interface="eth0:2",
+                  net_type="direct", dht_node=node_dht, debug=1)
+node_direct.start()
 
-#Start Bob's direct server.
-bob_dht = DHT()
-bob_direct = Net(passive_bind="192.168.0.44", passive_port=44445, interface="eth0:1", net_type="direct", node_type="active", dht_node=bob_dht, debug=1)
-bob_direct.start()
+# Start client direct connect.
+client_dht = DHT()
+client_direct = Net(passive_bind="192.168.0.44", passive_port=44445, interface="eth0:1",
+                    net_type="direct", node_type="active", dht_node=client_dht, debug=1)
+client_direct.start()
 
-#Callbacks.
+
+# Callbacks.
 def success(con):
     print("Alice successfully connected to Bob.")
     con.send_line("Sup Bob.")
 
+
 def failure(con):
-print("Alice failed to connec to Bob\a")
+    print("Alice failed to connec to Bob\a")
 
 events = {
     "success": success,
     "failure": failure
 }
 
-#Have Alice connect to Bob.
-alice_direct.unl.connect(bob_direct.unl.construct(), events)
+# Have the server connect to client.
+node_direct.unl.connect(client_direct.unl.construct(), events)
 
-#Event loop.
+# Event loop.
 while 1:
-#Bob get reply.
-for con in bob_direct:
-    for reply in con:
-        print(reply)
+    # client get reply.
+    for con in client_direct:
+        for reply in con:
+            print(reply)
 
-#Alice accept con.
-for con in alice_direct:
-    x = 1
+    # Server node accept con.
+    for con in node_direct:
+        x = 1
 
 time.sleep(0.5)
