02800c1bd7659b8cfc15fcf7c95c95908a938d08
Michael <michaelg@6ft.com>
2016-10-30 19:03:22 -0500

    new client code, fixed some bugs the the main

--- node/main.py
+++ node/main.py
@@ -9,12 +9,14 @@ ip_address_counter = 0
 
 
 def main():
+    global ip_address_counter
     # def _main():
     while 1:
         for x in range(settings.local_nodes):
             pid = os.fork()
             if pid == 0:
-                child()
+                ip_address_counter += 1
+                child(ip_address_counter)
         os.waitpid(pid, 0)
         print('parent')
 
--- node/process.py
+++ node/process.py
@@ -2,10 +2,8 @@
 from pyp2p.dht_msg import DHT
 from pyp2p.net import *
 
-def child():
+def child(ip_address_counter):
     print('child')
-    global ip_address_counter
-    ip_address_counter += 1
     node_dht = DHT()
     node = Net(passive_bind="192.168.0.4{}".format(ip_address_counter), passive_port=44444, interface="eth0:2",
                net_type="passive", dht_node=node_dht, debug=1)
